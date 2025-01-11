59894f14d957db7748b7dabb9c65dc053c320325
Alberto Martinelli s329095 <s329095@studenti.polito.it>
2025-01-11 11:04:23 +0100

    Final

--- 7_streamlit_app.py
+++ 7_streamlit_app.py
@@ -2,9 +2,14 @@ import streamlit as st
 import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pandas as pd
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
 
 def rating_prediction(input_review):
-    df = pd.read_csv('1_data_cleaned.csv')
+    df = pd.read_csv('data_cleaned.csv')
+    df = df.dropna(subset=['score'])
+
     df['review_cleaned_joined'] = df['review_cleaned_joined'].fillna("").astype(str)
     tfidf = TfidfVectorizer()
     tfidf.fit_transform(df['review_cleaned_joined'])
@@ -21,6 +26,29 @@ def rating_prediction(input_review):
 
     # print(f"Predicted Rating for the example review using the loaded model: {predicted_rating[0]}")
     return predicted_rating
+
+# Function to predict sentiment for a randomly generated review
+def predict_sentiment(review, tokenizer, model):
+    # Text Preprocessing (tokenization and padding)
+    review_sequence = tokenizer.texts_to_sequences([review])
+    review_padded = pad_sequences(review_sequence, padding='post', maxlen=50)
+
+    # Predict sentiment (0 for negative, 1 for positive)
+    prediction = model.predict(review_padded)
+    sentiment = "positive" if prediction >= 0.5 else "negative"
+    return sentiment
+
+def sentiment_prediction(review):
+    df = pd.read_csv('sentiment_analysis_results.csv')
+    model_save_path = "sentiment_analysis_model.h5"
+    model = load_model(model_save_path)
+
+    tokenizer = Tokenizer(num_words=5000)
+    tokenizer.fit_on_texts(df['review_cleaned_joined'])  # Ensure the tokenizer is fit on the same training data
+    sentiment = predict_sentiment(review, tokenizer, model)
+    return sentiment
+
+
 
 # Title
 st.title("NLP Project 2 - Supervised")
@@ -49,5 +77,5 @@ elif section == "Sentiment analysis":
 
     # Model Prediction
     if st.button("Predict Sentiment"):
-        prediction = model(review)  # Replace with your model inference logic
-        st.write(f"Predicted Sentiment: {prediction['label']}")
+        prediction = sentiment_prediction(review)  # Replace with your model inference logic
+        st.write(f"Predicted Sentiment: {prediction}")
